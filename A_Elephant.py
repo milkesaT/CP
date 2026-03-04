728dc304fb3af799b4a5fd0fb9bb3e512b272f51
Milkesa <milkesatesemawakwaya@gmail.com>
2026-03-04 16:40:23 +0300

    codeforce

---
+++ A_Elephant.py
@@ -0,0 +1,7 @@
+n=int(input())
+s=(n)//5
+r=s+1
+if (n%5==0):
+    print(s)
+else:
+    print(s+1)
---
+++ A_Petya_and_Strings.py
@@ -0,0 +1,8 @@
+n=str(input()).strip().upper()
+s=str(input()).strip().upper()
+if n==s:
+    print(0)
+elif n>s:
+    print(1)
+else:
+    print(-1)
