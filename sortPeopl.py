179e7055d7e92719bb66c18b3aa96db23378976f
Milkesa Tesema <milkesatesemawakwaya@gmail.com>
2026-03-19 23:57:31 +0300

    sort Peoplle  #Insertion method

---
+++ sortPeopl.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        n = len(heights)
+        for i in range(1, n):
+            key_height = heights[i]
+            key_name = names[i]
+            j = i-1
+            while j >= 0 and heights[j] < key_height:
+                heights[j + 1] = heights[j]
+                names[j + 1] = names[j]
+                j -= 1
+            heights[j + 1] = key_height
+            names[j + 1] = key_name
+        return names
