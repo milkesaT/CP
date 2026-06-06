fe39653c67dfd71cea6ff8f0693c83b8c690bac8
Milkesa Tesema <milkesatesemawakwaya@gmail.com>
2026-06-06 15:00:19 +0300

    Time: 4437 ms (52.83%), Space: 19 MB (99.06%) - LeetHub

---
+++ 3179-find-the-n-th-value-after-k-seconds/3179-find-the-n-th-value-after-k-seconds.py
@@ -0,0 +1,12 @@
+class Solution:
+    def valueAfterKSeconds(self, n: int, k: int) -> int:
+        nar=[1]*n
+        
+       
+       
+        for l in range(k):
+            for i in range(1,n):
+                MOD = 10**9 + 7
+                nar[i] = (nar[i-1] + nar[i]) % MOD
+        return nar[n-1]
+
